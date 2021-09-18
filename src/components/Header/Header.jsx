bd09b0c2fe730f713f849d28597d84ff21eaf859
khorh <helen.khor@googlemail.com>
2021-09-18 09:38:14 +0100

    completed styling of header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,10 @@
+import "./_header.scss";
+
 const Header = () => {
     return (
         <header>
-            <h1>PUNK!</h1>
-            <h2>Find out about Brewdog's craft beer</h2>
+            <h1 className="h1">PUNK BEERS</h1>
+            <h2 className="h2">Find out about Brewdog's craft beer</h2>
         </header>
     );
 };
