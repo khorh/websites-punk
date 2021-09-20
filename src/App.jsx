7bb1266ab4a21efcbe430f9ca59cdcdf2e084a2f
khorh <helen.khor@googlemail.com>
2021-09-20 16:06:14 +0100

    updated data with all 25 beers

--- src/App.jsx
+++ src/App.jsx
@@ -71,15 +71,15 @@ const App = () => {
     // Output
     const beerCard = beers.map(beer => (
         <article className="card" key={beer.id}>
-            <div>
+
                 <img className="card__image" src={beer.image_url} alt={beer.name} />
                 <h3 className="card__name">{beer.name}</h3>
-            </div>
+
             <h4 className="card__tagline">{beer.tagline}</h4>
-            <div>
+
                 <p className="card__text">{beer.first_brewed}</p>
                 <p className="card__text">abv {beer.abv}% | ph{beer.ph}</p>
-            </div>
+
         </article>
     ));
 
